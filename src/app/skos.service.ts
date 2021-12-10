c6927f4904e8930a8183fabe7bdb244c60d8f9fc
TSGames1 <anfragen@tsgames.de>
2021-12-10 15:58:43 +0100

    primary title

--- src/app/skos.service.ts
+++ src/app/skos.service.ts
@@ -19,7 +19,7 @@ export class SKOSService {
   async fetchSKOS(url: string) {
     let skos = await this.http.get<any>(url).toPromise();
     const nodes = [];
-    this.transform(skos.hasTopConcept, nodes);
+    this.transform(skos.title.de, skos.hasTopConcept, nodes);
     return nodes;
   }
   async getLinkStructure(url: string) {
@@ -29,8 +29,6 @@ export class SKOSService {
     //skos = await this.http.get<any>('https://vocabs.openeduhub.de/w3id.org/openeduhub/vocabs/learningResourceType/index.json').toPromise();
     //this.transform(skos.hasTopConcept, nodes);
 
-    console.log(nodes);
-
     return await this.buildLinks(nodes);
   }
   async buildLinks(nodes: any[]) {
@@ -39,7 +37,6 @@ export class SKOSService {
     nodes.forEach((element) => {
       SKOSService.SUPPORTED_LINKS.forEach((link) => {
         if (element.skos[link.id]) {
-          console.log(element);
           element.skos[link.id].forEach((target: any) => {
             // const target = nodes.filter((n) => n.id === related)[0];
             links.push({
@@ -55,31 +52,41 @@ export class SKOSService {
         }
       });
     });
+    console.log(urls);
+    console.log(links);
     for (const url of urls) {
       nodes = nodes.concat(await this.fetchSKOS(url));
     }
+    links.forEach(l => {
+      if(!nodes.find(n => n.id === l.target)) {
+        console.error('Invalid link target found', l);
+        links.splice(links.indexOf(l), 1);
+      }
+    });
+    console.log(nodes);
     return {
       nodes,
       links
     };
   }
 
-  transform(skos: any[], flat: any[], parent: any[] = []) {
+  transform(title: string, skos: any[], flat: any[], parent: any[] = []) {
     console.log(skos);
     skos.forEach((element) => {
       if (element.narrower) {
         const parentCopy = parent.slice();
         parentCopy.push(element);
-        this.transform(element.narrower, flat, parentCopy);
+        this.transform(title, element.narrower, flat, parentCopy);
       }
-      let path = parent.map((p) => p.prefLabel.de).join(" -> ");
+      let path = parent.map((p) => p.prefLabel.de);
+      path.splice(0, 0, title);
       if (path) {
         // path += ' -> ';
       }
       flat.push({
         id: element.id,
         label: element.prefLabel.de,
-        path: path,
+        path: path.join(' -> '),
         skos: element,
       });
     });
