82e0d9939db79b23e2ed3bb9b9d17a48d88db824
Torsten Simon <simon@edu-sharing.net>
2022-01-10 18:04:23 +0100

    add query params

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SKOSService } from './skos.service';
+import {ActivatedRoute, Params, Route, Router} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {skip} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -11,23 +14,49 @@ export class AppComponent implements OnInit {
   title = 'graph';
   readonly window = window;
   data: any;
-  url = 'https://vocabs.openeduhub.de/w3id.org/openeduhub/vocabs/new_lrt/index.json';
-  link = '';
-  vocabId = '';
-  hideEmpty = false;
+  queryParams: Params = {
+    url: '',
+    link: '',
+    vocabId: '',
+    hideEmpty: false
+  };
+  form: FormGroup;
   constructor(
-    private skos: SKOSService
+    private skos: SKOSService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder,
   ) {
+    this.form = this.formBuilder.group({
+      url: ['', Validators.required],
+      link: [''],
+      vocabId: [''],
+      hideEmpty: [false],
+    });
+    this.form.valueChanges.pipe(skip(1)).subscribe((v) => {
+      this.router.navigate([], {
+        relativeTo: this.route,
+        queryParamsHandling: 'merge',
+        queryParams: v
+      });
+    });
+    this.route.queryParams.subscribe((q) => {
+      q = JSON.parse(JSON.stringify(q));
+      console.log(q);
+      q.url = q.url ?? 'https://vocabs.openeduhub.de/w3id.org/openeduhub/vocabs/new_lrt/index.json';
+      this.queryParams = q;
+      this.form.patchValue(q);
+    });
   }
   async ngOnInit() {
 
   }
 
   async loadSKOS() {
-    this.data = await this.skos.getLinkStructure(this.url, {
-      link: this.link,
-      vocabId: this.vocabId,
-      hideEmpty: this.hideEmpty,
+    this.data = await this.skos.getLinkStructure(this.queryParams.url, {
+      link: this.queryParams.link,
+      vocabId: this.queryParams.vocabId,
+      hideEmpty: this.queryParams.hideEmpty,
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { NgxGraphModule } from '@swimlane/ngx-graph';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([]),
     HttpClientModule,
     NgxGraphModule,
     BrowserAnimationsModule,
@@ -25,6 +27,7 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
     MatInputModule,
     MatSelectModule,
     MatCheckboxModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
